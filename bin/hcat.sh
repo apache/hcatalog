67ea3d03985cdd511291d9fcb68bcd4147cf4bc2
Ashutosh Chauhan <hashutosh@apache.org>
2011-06-01 16:41:38 +0000

    HCATALOG-20: Create a package target. On branch 0.1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hcatalog/branches/branch-0.1@1130216 13f79535-47bb-0310-9956-ffa450edef68

--- bin/hcat.sh
+++ bin/hcat.sh
@@ -16,23 +16,78 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-HOWL_DIR=`dirname "$0"`
+# Resolve our absolute path                                                      
+# resolve links - $0 may be a softlink                                           
+this="${BASH_SOURCE-$0}"                                                         
+while [ -h "$this" ]; do                                                         
+    ls=`ls -ld "$this"`                                                          
+    link=`expr "$ls" : '.*-> \(.*\)$'`                                           
+    if expr "$link" : '.*/.*' > /dev/null; then                                  
+        this="$link"                                                             
+    else                                                                         
+        this=`dirname "$this"`/"$link"                                           
+    fi                                                                           
+done                                                                             
+                                                                                 
+# convert relative path to absolute path                                         
+bin=`dirname "$this"`                                                            
+script=`basename "$this"`                                                        
+bin=`unset CDPATH; cd "$bin"; pwd`                                               
+this="$bin/$script"                                                              
+                                                                                 
+# the root of the HCatalog installation                                          
+export HCAT_HOME=`dirname "$this"`/..                                            
 
-HOWL_JAR_LOC=`find . -name "hcatalog*.jar"`
+# filter debug command line parameter
+debug=false
 
-HADOOP_CLASSPATH=$HADOOP_CLASSPATH:${HOWL_JAR_LOC}:../lib/commons-cli-2.0-SNAPSHOT.jar:../build/cli/hive-cli-0.7.0.jar:../ql/lib/antlr-runtime-3.0.1.jar
+for f in $@; do
+     if [[ $f = "-secretDebugCmd" ]]; then
+        debug=true
+     else
+        remaining="${remaining} $f"
+     fi
+done
+
+# Find our hcatalog jar
+HCAT_JAR=$HCAT_HOME/lib/hcatalog-*.jar
+
+# Add all of the other jars to our classpath
+for jar in $HCAT_HOME/lib/*.jar ; do
+	HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$jar
+done
+
+# Put our config file in the classpath
+HADOOP_CLASSPATH=${HADOOP_CLASSPATH}:${HCAT_HOME}/conf/hive-site.xml
 
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
 
-for f in `ls ../build/dist/lib/*.jar`; do 
-  HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$f 
-done
+# Find our Thrift address from the config file
+THRIFT=`sed -n '/<name>hive.metastore.uris<\/name>/ {
+      n
+      s/.*<value>\(.*\)<\/value>.*/\1/p
+      }' $HCAT_HOME/conf/hive-site.xml`
+HADOOP_OPTS="$HADOOP_OPTS -Dhive.metastore.uris=$THRIFT " 
 
-HADOOP_OPTS="$HADOOP_OPTS -Dhive.metastore.uris=thrift://localhost:9083 " 
+# Find our Warehouse dir from the config file
+WAREHOUSE_DIR=`sed -n '/<name>hive.metastore.warehouse.dir<\/name>/ {
+      n
+      s/.*<value>\(.*\)<\/value>.*/\1/p
+      }' $HCAT_HOME/conf/hive-site.xml`
+HADOOP_OPTS="$HADOOP_OPTS -Dhive.metastore.warehouse.dir=$WAREHOUSE_DIR " 
+
 
 export HADOOP_OPTS=$HADOOP_OPTS
 
-exec $HADOOP_HOME/bin/hadoop jar  ${HOWL_JAR_LOC} org.apache.hcatalog.cli.HCatCli "$@"
+# run it
+if [ "$debug" == "true" ]; then
+	echo "Would run:"
+	echo "exec $HADOOP_HOME/bin/hadoop jar $HCAT_JAR org.apache.hcatalog.cli.HCatCli $remaining"
+	echo "with HADOOP_CLASSPATH set to ($HADOOP_CLASSPATH)"
+	echo "and HADOOP_OPTS set to ($HADOOP_OPTS)"
+else
+	exec $HADOOP_HOME/bin/hadoop jar  $HCAT_JAR org.apache.hcatalog.cli.HCatCli $remaining
+fi
 
 # Above is the recommended way to launch hcatalog cli. If it doesnt work, you can try the following:
-# java -Dhive.metastore.uris=thrift://localhost:9083 -cp ../lib/commons-logging-1.0.4.jar:../build/hadoopcore/hadoop-0.20.0/hadoop-0.20.0-core.jar:../lib/commons-cli-2.0-SNAPSHOT.jar:../build/cli/hive-cli-0.7.0.jar:../ql/lib/antlr-runtime-3.0.1.jar:$HOWL_JAR org.apache.hcatalog.cli.HCatCli "$@"
+# java -Dhive.metastore.uris=thrift://localhost:9083 -cp ../lib/commons-logging-1.0.4.jar:../build/hadoopcore/hadoop-0.20.0/hadoop-0.20.0-core.jar:../lib/commons-cli-2.0-SNAPSHOT.jar:../build/cli/hive-cli-0.7.0.jar:../ql/lib/antlr-runtime-3.0.1.jar:$HCAT_JAR_LOC org.apache.hcatalog.cli.HCatCli "$@"
