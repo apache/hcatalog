6ac672ab903dfc17f168cf690dd62f68a8f89f5b
Travis Crawford <travis@apache.org>
2012-12-07 01:26:26 +0000

    HCATALOG-558 Update test.sh to test from an extracted src-release
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hcatalog/trunk@1418158 13f79535-47bb-0310-9956-ffa450edef68

--- build-support/scripts/test.sh
+++ build-support/scripts/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Run HCatalog tests. This script intends to centralize the different commands
+# and options necessary to build and test HCatalog. It should be run before
+# committing patches, and from CI.
+
 function run_cmd() {
   echo "Running command: ${cmd}"
   ${cmd}
@@ -9,14 +13,31 @@ function run_cmd() {
   fi
 }
 
+if [ "${FORREST_HOME}" == "" ]; then
+  echo "required environment variable FORREST_HOME not set"
+  exit 1
+fi
+
 umask 0022
+env
+
+cmd='ant clean src-release'
+run_cmd
+
+cd build
+tar -xzvf hcatalog-src-*.tar.gz
+cd hcatalog-src-*
+echo "Running tests from $(pwd)"
 
 # Build with hadoop23, but do not run tests as they do not pass.
-cmd='ant clean package -Dmvn.hadoop.profile=hadoop23'
+cmd='ant -v clean package -Dmvn.hadoop.profile=hadoop23'
 run_cmd
 
 # Build and run tests with hadoop20. This must happen afterwards so test results
 # are available for CI to publish.
-cmd='ant clean package test -Dtest.junit.output.format=xml'
+cmd='ant -v -Dtest.junit.output.format=xml clean package test'
+if [ "${HUDSON_URL}" == "https://builds.apache.org/" ]; then
+  cmd="${cmd} mvn-deploy"
+fi
 run_cmd
 
