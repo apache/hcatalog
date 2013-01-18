e621b5b7caff1792eeb8f39e290115259e6a6c08
Travis Crawford <travis@apache.org>
2013-01-18 22:32:40 +0000

    HCATALOG-603 releaseaudit should run before generating artifacts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hcatalog/branches/branch-0.5@1435378 13f79535-47bb-0310-9956-ffa450edef68

--- build-support/scripts/test.sh
+++ build-support/scripts/test.sh
@@ -55,7 +55,7 @@ run_cmd
 
 # Build and run tests with hadoop20. This must happen afterwards so test results
 # are available for CI to publish.
-cmd='ant -Dtest.junit.output.format=xml clean package releaseaudit test'
+cmd='ant -Dtest.junit.output.format=xml clean releaseaudit package test'
 if [ "${HUDSON_URL}" == "https://builds.apache.org/" ]; then
   cmd="${cmd} mvn-deploy"
 fi
